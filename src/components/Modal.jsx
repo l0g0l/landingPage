124d68ace68977a46fc9850b0ce94f8a7635b886
Lucia González <luciag.lara@gmail.com>
2021-11-05 15:58:14 +0100

    animation navmenu, don't work well

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -14,39 +14,31 @@ function Modal({ content }) {
         <div className="title" style={{ marginBottom: content.mbt, letterSpacing: content.ls, fontSize: content.fs }} >
           <p>{content.title}</p>
         </div>
-        <div className="body" style={{ display: content.displayflex}} >
+        <div className="body" style={{ display: content.displayflex }} >
           <div>
-
             <span>{content.text}
               <br />
 
             </span>
           </div>
-       
-            <div className="card" style={{ width: content.width, height:content.height, marginLeft:content.ml, marginTop: content.mt }}>
-              <div className="front">
 
-                <img src={content.img} alt={content.title} className="img-modal" style={{ width: content.width,  marginRight: content.mr }} />
+          <div className="card" style={{ width: content.width, height: content.height, marginLeft: content.ml, marginTop: content.mt }}>
+            <div className="front">
+
+              <img src={content.img} alt={content.title} className="img-modal" style={{ width: content.width, marginRight: content.mr }} />
+            </div>
+            <div className="back">
+              <div>
+                <p><strong>Tecnologías:</strong></p>
               </div>
-              <div className="back">
-                <div>
-
-                  <p><strong>Tecnologías:</strong></p>
-                </div>
-                <div>
-
-                  <p>  {content.tecnolog}</p>
-                </div>
-
+              <div>
+                <p>  {content.tecnolog}</p>
               </div>
             </div>
-
-
-
+          </div>
         </div>
         <div className="footer">
           <a href={content.url} target="_blank" rel="noreferrer noopener">View Web Application</a>
-
         </div>
       </div>
     </div>
